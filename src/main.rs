112b5140ed67b927bb0f2163356d95f87a3d131d
yozhgoor <yozhgoor@outlook.com>
2021-02-09 15:20:35 +0100

    Add the main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate base64;
+extern crate rpassword;
+extern crate sha1;
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<_>>();
+    let cmd = &args[0];
+
+    match &args[1..] {
+        [site] if site != "--help" => println!("{}", make_password(&ask_password().unwrap(), site)),
+        [] => println!(
+            "{}",
+            make_password(&ask_password().unwrap(), &ask_site().unwrap())
+        ),
+        _ => usage(cmd),
+    }
 }
