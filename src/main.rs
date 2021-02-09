79422a8b8860c572c92ab49892dc7aec1c69150e
yozhgoor <yozhgoor@outlook.com>
2021-02-09 15:38:12 +0100

    Add the generate_password function

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,17 @@ fn make_password(password: &str, site: &str) -> String {
 
     generate_password(&b64)
 }
+
+fn generate_password(s: &str) -> String {
+    let bytes = s.as_bytes();
+    let symbols: &[u8] = "!?+-=*/@#$%&()[];:,.<>".as_bytes();
+
+    format!(
+        "{}{}{}{}{}",
+        ("A".as_bytes()[0] + bytes[0] % 26) as char,
+        ("a".as_bytes()[0] + bytes[1] % 26) as char,
+        ("0".as_bytes()[0] + bytes[2] % 10) as char,
+        symbols[bytes[3] as usize % symbols.len()] as char,
+        s.get(4..26).unwrap(),
+    )
+}
